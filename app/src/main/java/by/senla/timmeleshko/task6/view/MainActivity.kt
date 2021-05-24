4d0ffc2122d2ae2407d2ee0a935e0c97b51e369a
Tim <tima.m.98@mail.ru>
2021-05-24 14:53:05 +0300

    Sixth lesson second commit

--- app/src/main/java/by/senla/timmeleshko/task6/view/MainActivity.kt
+++ app/src/main/java/by/senla/timmeleshko/task6/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package by.senla.timmeleshko.task6.view
 
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -9,6 +10,9 @@ import by.senla.timmeleshko.task6.R
 import by.senla.timmeleshko.task6.adapters.RecyclerViewAdapter
 import by.senla.timmeleshko.task6.model.Constants.GRID_LAYOUT_COLUMNS_COUNT
 import by.senla.timmeleshko.task6.model.network.DataViewModel
+import com.rubensousa.decorator.ColumnProvider
+import com.rubensousa.decorator.GridMarginDecoration
+import com.rubensousa.decorator.LinearMarginDecoration
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +37,18 @@ class MainActivity : AppCompatActivity() {
     private fun initListView() {
         recyclerView.apply {
             layoutManager = GridLayoutManager(this@MainActivity, GRID_LAYOUT_COLUMNS_COUNT)
+            addItemDecoration(GridMarginDecoration(
+                verticalMargin = dpToPx(16f),
+                horizontalMargin = dpToPx(16f),
+                columnProvider = object : ColumnProvider {
+                    override fun getNumberOfColumns(): Int = GRID_LAYOUT_COLUMNS_COUNT
+                },
+                orientation = RecyclerView.VERTICAL
+            ))
             adapter = this@MainActivity.adapter
         }
     }
+
+    private fun dpToPx(dp: Float): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+        resources.displayMetrics).toInt()
 }
